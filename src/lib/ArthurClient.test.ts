34767295f737cfab59f4705e4164c49aa7e09316
philippefutureboy <philippe@human.space>
2019-10-12 14:01:18 -0400

    feat(src/lib/arthurclient): barebone ArthurClient with minimal tests implemented
    Implemented a barebone ArthurClient which is a simple wrapper for fetch using the ApolloClient
    interface

---
+++ src/lib/ArthurClient.test.ts
@@ -0,0 +1,109 @@
+import 'jest';
+import ArthurClient, { ArthurClientContext } from './ArthurClient';
+
+function testInvalidContext(fn) {
+  test('given invalid context, should throw error', () => {
+    // act
+    // assert
+    // @ts-ignore
+    expect(() => fn({})).toThrow(
+      new Error('headers is a required field')
+    );
+    // @ts-ignore
+    expect(() => fn({ headers: {} })).toThrow(
+      new Error('tenant is a required field')
+    );
+    // @ts-ignore
+    expect(() => fn({ tenant: 'tenant', headers: {} })).toThrow(
+      new Error('tenant must be a valid UUIDv5')
+    );
+    expect(
+      () =>
+        // @ts-ignore
+        fn({
+          tenant: 'a6edc906-2f9f-5fb2-a373-efac406f0ef2',
+          uri: 'uri',
+          headers: {}
+        })
+    ).toThrow(new Error('uri must be a valid URL'));
+    expect(
+      () =>
+        // @ts-ignore
+        fn({
+          tenant: 'a6edc906-2f9f-5fb2-a373-efac406f0ef2',
+          uri: 'https://example.com',
+          headers: {}
+        })
+    ).not.toThrow();
+    expect(
+      () =>
+        // @ts-ignore
+        fn({
+          tenant: 'a6edc906-2f9f-5fb2-a373-efac406f0ef2',
+          headers: false
+        })
+    ).toThrow(
+      new Error(
+        'headers must be a `object` type, but the final value was: `null` (cast from the value `false`).\n If "null" is intended as an empty value be sure to mark the schema as `.nullable()`'
+      )
+    );
+    expect(
+      () =>
+        // @ts-ignore
+        fn({
+          tenant: 'a6edc906-2f9f-5fb2-a373-efac406f0ef2',
+          fetch: false,
+          headers: {}
+        })
+    ).toThrow(new Error('fetch must be a function'));
+  });
+}
+
+describe('constructor', () => {
+  testInvalidContext((value: ArthurClientContext) => new ArthurClient(value));
+
+  test('given valid context, should assign them as context', () => {
+    // arrange
+    const options = {
+      tenant: 'a6edc906-2f9f-5fb2-a373-efac406f0ef2',
+      headers: {}
+    };
+
+    // act
+    const client = new ArthurClient(options);
+
+    // assert
+    expect(client.context).toEqual(options);
+  });
+})
+
+describe('setContext', () => {
+  testInvalidContext((value: ArthurClientContext) => (new ArthurClient({
+      tenant: 'a6edc906-2f9f-5fb2-a373-efac406f0ef2',
+      headers: {}
+  })).setContext(value));
+
+  test('given valid context, should assign them as context', () => {
+    // arrange
+    const options = {
+      tenant: 'a6edc906-2f9f-5fb2-a373-efac406f0ef2',
+      headers: {}
+    };
+    const context = {
+      tenant: '9342d47a-1bab-5709-9869-c840b2eac501',
+      headers: {}
+    };
+
+    // act
+    const client = new ArthurClient(options);
+    client.setContext(context);
+
+    // assert
+    expect(client.context).toEqual(context);
+  });
+});
+
+// TODO: test query
+
+
+
---
+++ src/lib/ArthurClient.ts
@@ -0,0 +1,82 @@
+import * as yup from 'yup';
+
+export interface ArthurClientContext {
+  readonly tenant: string;
+  // review types here
+  readonly headers: any;
+  readonly uri?: string;
+  // review types here by using either https://github.com/developit/unfetch/blob/master/src/index.d.ts
+  // or node-fetch types
+  readonly fetch?: any;
+}
+
+const REGEX_UUID_V5 = /^[0-9A-F]{8}-[0-9A-F]{4}-[5][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+const ARTHUR_CLIENT_CONTEXT_SCHEMA = yup.object().shape({
+  tenant: yup
+    .string()
+    .required()
+    .test('isuuidv5', 'tenant must be a valid UUIDv5', function(value) {
+      return REGEX_UUID_V5.test(value);
+    }),
+  uri: yup.string().url(),
+  headers: yup.object().required(),
+  fetch: yup
+    .mixed()
+    .test('isFunction', 'fetch must be a function', function(value) {
+      return value === undefined || typeof value === 'function';
+    })
+});
+
+export default class ArthurClient {
+
+  public context: ArthurClientContext;
+
+  constructor(options: ArthurClientContext) {
+    if(ARTHUR_CLIENT_CONTEXT_SCHEMA.validateSync(options)) {
+      this.context = { ...options };
+    }
+  }
+
+  public setContext(context: ArthurClientContext): void {
+    if (ARTHUR_CLIENT_CONTEXT_SCHEMA.validateSync(context)) {
+      this.context = { ...context };
+    }
+  }
+
+  public async query({ query, context }: { readonly query: any, readonly context: ArthurClientContext}): Promise<any> {
+    var currentContext = this.context;
+    if (context && ARTHUR_CLIENT_CONTEXT_SCHEMA.validateSync(context)) {
+      currentContext = context;
+    }
+    if (typeof query !== 'object' || query === null) {
+      throw new Error('query should be an object');
+    }
+    const {
+      fetch = window && window.fetch ? window.fetch : nofetch,
+      headers = {},
+      tenant,
+      uri
+    } = currentContext;
+
+    const response: Response = await fetch(
+      `${uri}?query=${encodeURIComponent(JSON.stringify(query))}`,
+      {
+        method: 'get',
+        headers: {
+          ...(headers || {}),
+          'arthur-tenantkey': tenant
+        }
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`${response.status} (${response.statusText})`);
+    }
+    return response.json();
+  }
+}
+
+
+function nofetch(): void {
+  throw new Error('Fetch API is unavailable and no custom fetch function was provided')
+}
